460e44dfb27832a0a4c1a85c2eda09cb2e3ad828
T. Lanette <TLPollard4516@gmail.com>
2022-05-07 19:56:51 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Book.js
+++ src/components/Book.js
@@ -30,7 +30,7 @@ const Book = ({ book, changeBookShelf}) => {
                 </div>
             </div>
             <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.publisher}</div>
+            <div className="book-authors">{book.authors}</div>
         </div>
     );
 };
--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -11,9 +11,10 @@ const BookShelf = ({ books, title, updateBookShelf }) => {
             <h2 className="bookshelf-title">{title}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                    {books.map(book => (
-                    <li key={book.id}>
+                    {books.map((book) => (
+                    <li>
                         <Book
+                            key={book.id}
                             book={book}
                             changeBookShelf={updateBookShelf}
                         />
